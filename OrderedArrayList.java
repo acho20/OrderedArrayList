29c3fa68891f3ebebbab29d1d2924f2ec2d4f17a
Alex Cho <acho20@stuy.edu>
2020-12-13 11:20:11 -0500

    added add() to OrderedArrayList.java

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,3 +1,11 @@
-class OrderedArrayList extends NoNullArrayList{
-  
+class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+  public boolean add(T x){
+    for(int i = 0; i < super.size(); i++){
+      if(x.compareTo(super.get(i)) < 0){
+        super.add(i, x);
+        return true;
+      }
+    }
+    return super.add(x);
+  }
 }
